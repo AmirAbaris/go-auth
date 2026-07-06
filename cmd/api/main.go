fbc83b9287c7de80be9b65d71f87be4b0d339e86
amir abaris <thisisamirabaris@gmail.com>
2026-07-06 18:03:48 +0330

    refactor: improve main function structure with detailed comments for server setup and shutdown

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -19,15 +19,20 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	// 1. handle config
 	cfg := config.LoadConfig()
+	// 2. handle mux
 	mux := http.NewServeMux()
+	// 3. hande routes
 	mux.HandleFunc("GET /hi", handler)
 
+	// 4. config server struct
 	server := &http.Server{
 		Addr:    ":" + cfg.Port,
 		Handler: mux,
 	}
 
+	// 5. server.listen in goroutine
 	go func() {
 		log.Printf("Server listening on %s", cfg.Port)
 
@@ -37,11 +42,13 @@ func main() {
 		}
 	}()
 
+	// 6. handle stop
 	stop := make(chan os.Signal, 1)
 	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
 
 	<-stop
 
+	// 7. handle context timeout
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
